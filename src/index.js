28c4e18f52ac7eab455bb757092908fa884236e3
Diego Alberto Ayala Vázquez <45373531+Diegooayalavaz@users.noreply.github.com>
2022-08-10 13:16:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const roote = ReactDOM.createRoot(document.getElementById('root'));
+const toot = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
